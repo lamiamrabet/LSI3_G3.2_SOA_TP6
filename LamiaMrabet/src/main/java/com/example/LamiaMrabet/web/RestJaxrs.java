8f27f156c3e354c774231a0af59c37d7fae2c91b
Lamia Mrabet <lamiamrabet456@gmail.com>
2023-12-01 10:17:49 +0100

    commit fin

--- LamiaMrabet/src/main/java/com/example/LamiaMrabet/web/RestJaxrs.java
+++ LamiaMrabet/src/main/java/com/example/LamiaMrabet/web/RestJaxrs.java
@@ -3,9 +3,7 @@ package com.example.LamiaMrabet.web;
 import com.example.LamiaMrabet.enteties.Compte;
 import com.example.LamiaMrabet.repositories.CompteRepository;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,8 +18,35 @@ public class RestJaxrs {
     @GET
     @Path("/comptes")
     @Transactional
-    @Produces({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<Compte> comptelist(){
         return cmt_rep.findAll();
+    }
+    @Path("/comptes/{id}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Compte getOne(@PathParam(value = "id") Long id){
+        return cmt_rep.findById(id).get();
+    }
+    @Path("/comptes")
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public  Compte save(Compte cmt){
+        return cmt_rep.save(cmt);
+    }
+    @Path("/comptes/{id}")
+    @PUT
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Compte update(Compte cmt, @PathParam("id") Long id){
+        cmt.setId(id);
+        return cmt_rep.save(cmt);
+    }
+    @Path("/comptes/{id}")
+    @DELETE
+    @Produces({MediaType.APPLICATION_JSON})
+    public void delete(@PathParam("id") Long id){
+        cmt_rep.deleteById(id);
     }
 }
